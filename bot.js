baba758499c0340b21e62c22c8af81e305e68ade
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-23 09:30:32 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,14 @@ client.on('ready', () => {
 
 });
 
-//Ignoring messages if not starting with prefix
-if (!message.content.startsWith('>') || message.author.bot) return; 
-
+ 
+//'check for this on new message'
 client.on('message', message => {
+    
+//Ignoring messages if not starting with prefix
+if (!message.content.startsWith('>') || message.author.bot) return;
 
+    //check if bot is working properly function
     if (message.content === 'ping') {
 
        message.reply('pong');
