07aaff4e858d9ba3451dbbf6a152904e439f9016
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-23 09:45:19 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.on('ready', () => {
 
     console.log('I am ready!');
@@ -12,8 +11,8 @@ client.on('ready', () => {
 //'check for this on new message'
 client.on('message', message => {
     
-//Ignoring messages if not starting with prefix
-if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+//Ignoring all messages BELOW if not starting with prefix
+if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 //ignores case and makes sure message can be read
 const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
