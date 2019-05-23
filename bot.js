227ed932f3ab60370c7ce155a7f7745c1b0e25bf
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-23 10:38:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,8 +37,31 @@ const command = args.shift().toLowerCase();
     
     //outlines everything bot can do
     if (command === 'help') {
-        
+        message.channel.send({embed: {
+        color: 10826248,
+        author: {
+          name: "Accurate Realization Gadget",
+          icon_url: client.user.avatarURL
+        },
+        title: "Type \a! followed by any of these commands to use them.",
+        description: "The Accurate Realization Gadget is a tool used for utility and fun.
+        fields: [{
+            name: "Fun:",
+            value: "` Nothing here yet. `"
+        },
+        {
+            name: "Utilities:",
+            value: "` help ` ` ping `"
+        },                
+         ],
+                             timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
     }
+       
+         }
+         }})
+        }
 
 });
 
