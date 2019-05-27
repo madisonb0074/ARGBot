f0853e7660a8a1e2e804c844d6c3ba96e81b3284
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-27 09:39:34 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ client.on('message', message => {
                     icon_url: client.user.avatarURL
                 },
                 title: "Type \a! followed by any of these commands to use them.",
-                description: "The Accurate Realization Gadget is a tool used for utility and fun.
+                description: "The Accurate Realization Gadget is a tool used for utility and fun.",
                 fields: [{
                     name: "Fun:",
                     value: "` Nothing here yet. `"
@@ -60,9 +60,9 @@ client.on('message', message => {
                     icon_url: client.user.avatarURL,
                 }
 
-            
-        
-    },}
+
+            }
+    }
 )
 }
 
@@ -72,4 +72,3 @@ client.on('message', message => {
 
 // Bot token hidden in external variable
 client.login(process.env.BOT_TOKEN);
-
