edf1e21536e42a433084f69a8bbfb610aaa02f9b
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-27 10:38:30 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 //response command, ping = filename, thus it recognizes ping as the command and responds.
 
 exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
-}
+    message.channel.send("pong!");
+};
