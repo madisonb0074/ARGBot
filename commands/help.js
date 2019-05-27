1bb9f9ba1cb6406f8e24054e3205b8e295a0acb2
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-27 10:07:30 -0600

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+exports.run = (client, message, args) => {
+    message.channel.send(message.channel.send({
+        embed: {
+            color: 10826248,
+            author: {
+                name: "Accurate Realization Gadget",
+                icon_url: client.user.avatarURL
+            },
+            title: "Type \a! followed by any of these commands to use them.",
+            description: "The Accurate Realization Gadget is a tool used for utility and fun.",
+            fields: [{
+                name: "Fun:",
+                value: "` Nothing here yet. `"
+            },
+                {
+                    name: "Utilities:",
+                    value: "` help ` ` ping `"
+                },
+            ],
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+            }
+
+
+        }
+    }).catch(console.error);
+}
