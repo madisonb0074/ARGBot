c17a3be4c0d2a335fb1283c30365a70f0383d77c
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-27 10:10:27 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,21 +1,20 @@
 module.exports = (client, message) => {
-//Ignoring all messages BELOW if not starting with prefix
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  // Ignore all bots
+  if (message.author.bot) return;
 
-//ignores case and makes sure message can be read
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+  // Ignore messages not starting with the prefix (in config.json)
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-    
+  // Our standard argument/command name definition.
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-});
-// grab the command data from Enmap
-const cmd = client.commands.get(command);
+  // Grab the command data from the client.commands Enmap
+  const cmd = client.commands.get(command);
 
-// if that command doesn't exist, exit and do nothing
-if (!cmd) return;
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) return;
 
-// Run the command
-cmd.run(client, message, args);
-}
-;
+  // Run the command
+  cmd.run(client, message, args);
+};
