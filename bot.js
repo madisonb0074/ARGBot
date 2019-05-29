5d737ad9690fcd807931aaaf328bd8384d1b2394
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-29 15:36:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
+	
+var channel = client.channels.get('581154704785014784');
+channel.sendMessage("I am online.");
+	
   client.user.setPresence({
     status: 'online',
     game: {name: "Use a!help!"}
@@ -10,15 +14,17 @@ client.on("ready", () => {
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+	if (message.author.bot) return;
+ 	// This is where we'll put our code.
+  	if (message.content.indexOf(config.prefix) !== 0) return;
+ 
+  	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  	const command = args.shift().toLowerCase();
 	
 	//ping command, responds pong when user says a!ping
 	if (command === 'ping') {
 
-       	message.reply('pong');
+       	message.channel.send('pong');
 
        	}
 
