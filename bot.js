01d8359fc81f79f81081b2bef03e55b07d243368
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-05-31 10:12:18 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,11 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-
 client.on("ready", () => {
-	
+
 var channel = client.channels.get('581154704785014784');
 channel.sendMessage("I am online.");
-	
+
   client.user.setPresence({
     status: 'online',
     game: {name: "Use a!help!"}
@@ -15,28 +14,19 @@ channel.sendMessage("I am online.");
 });
 
 client.on('message', message => {
-	//if the message sender is a bot, ignore it to prevent feedback loop
 	if (message.author.bot) return;
+ 	// This is where we'll put our code.
   	if (message.content.indexOf(config.prefix) !== 0) return;
- 
+
   	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   	const command = args.shift().toLowerCase();
-	
+
 	//ping command, responds pong when user says a!ping
 	if (command === 'ping') {
 
        	message.channel.send('pong');
 
-       	} else if (command === 'channel'){
-	
-	var server = message.guild;
-	//server name is bot username
-	var name = message.author.username;
-	//creating a channel
-	server.createChannel(name, "text");
-		
 	}
-	
 
 });
 
