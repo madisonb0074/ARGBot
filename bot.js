a23b3b4aad69d71f189b5b1e70693b0386e876d4
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-06-01 14:49:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,17 +42,7 @@ client.on('messageDelete', async (message) => {
   if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
     console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
   }
-  const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
-  let user = ""
-    if (entry.extra.channel.id === message.channel.id
-      && (entry.target.id === message.author.id)
-      && (entry.createdTimestamp > (Date.now() - 5000))
-      && (entry.extra.count >= 1)) {
-    user = entry.executor.username
-  } else {
-    user = message.author.username
-  }
-  const channel = messageDelete.guild.channels.find(ch => ch.name === 'accuratebotlog');channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author} was deleted. There ID is ${messageDelete.author.id}`)});
+  const channel = messageDelete.guild.channels.find(ch => ch.name === 'accuratebotlog');channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author} was deleted. Their ID is ${messageDelete.author.id}`)});
 })
 
 
