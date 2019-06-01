0f488bd3390f6eae4f203f2fa5828ca0a9932855
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-06-01 15:02:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const config = require("./config.json");
 client.on("ready", () => {
 
 var channel = client.channels.get('581154704785014784');
-channel.sendMessage("I am online.");
+channel.sendMessage('I am online.');
 
   client.user.setPresence({
     status: 'online',
@@ -33,16 +33,24 @@ client.on('message', message => {
 
 //audit log for bot, first sends message when
 client.on('messageDelete', async (message) => {
-  //Creating a log channel within a server if has the permissions
-  const logs = message.guild.channels.find(channel => channel.name === "accuratebotlog");
-  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+  const accuratebotlog = message.guild.channels.find(channel => channel.name === "accuratebotlog");
+  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlog) {
     message.guild.createChannel('accuratebotlog', 'text');
   }
-  //If doesn't have the permissions, complain about it in console console.log();
-  if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+  if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlog) {
     console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
   }
-  const channel = messageDelete.guild.channels.find(ch => ch.name === 'accuratebotlog');channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author} was deleted. Their ID is ${messageDelete.author.id}`)});
+  const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
+  let user = ""
+    if (entry.extra.channel.id === message.channel.id
+      && (entry.target.id === message.author.id)
+      && (entry.createdTimestamp > (Date.now() - 5000))
+      && (entry.extra.count >= 1)) {
+    user = entry.executor.username
+  } else {
+    user = message.author.username
+  }
+  accuratebotlog.sendMessage(`A message was deleted in ${message.channel.name} by ${user}`);
 })
 
 
