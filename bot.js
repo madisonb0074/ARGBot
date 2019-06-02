a06e8a261994d37a74131958c412020d66069c4e
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-06-02 13:11:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,57 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.json");
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
 
-
-client.on("ready", () => {
-
-var channel = client.channels.get('581154704785014784');
-channel.sendMessage('I am online.');
+client.on('ready', () => {
+  var channel = client.channels.get('581154704785014784')
+  channel.sendMessage('I am online.')
 
   client.user.setPresence({
     status: 'online',
-    game: {name: "Use a!help!"}
-  });
-});
+    game: {
+      name: 'Use a!help!'
+    }
+  })
+})
 
 client.on('message', message => {
-	//if the message sender is a bot, ignore it to prevent feedback loop
-	if (message.author.bot) return;
-  	if (message.content.indexOf(config.prefix) !== 0) return;
+  // if the message sender is a bot, ignore it to prevent feedback loop
+  if (message.author.bot) return
+  if (message.content.indexOf(config.prefix) !== 0) return
 
-  	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  	const command = args.shift().toLowerCase();
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
 
-	//ping command, responds pong when user says a!ping
-	if (command === 'ping') {
+  // ping command, responds pong when user says a!ping
+  if (command === 'ping') {
+    message.channel.send('pong')
+  }
+})
 
-       	message.channel.send('pong');
-
-       	}
-
-});
-
-//audit log for bot, first sends message when
+// audit log for bot, first sends message when anything is deleted
 client.on('messageDelete', async (message) => {
-  const accuratebotlog = message.guild.channels.find(channel => channel.name === "accuratebotlog");
-  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlog) {
-    message.guild.createChannel('accuratebotlog', 'text');
+  // find the logs channel and check if bot has any permissions to MANAGE_CHANNELS
+  const accuratebotlogs = message.guild.channels.find(channel => channel.name === 'accuratebotlogs')
+  if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlogs) {
+    message.guild.createChannel('accuratebotlogs', 'text')
   }
-  if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlog) {
-    console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
+  if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !accuratebotlogs) {
+    console.log('The accuratebotlogs channel does not exist and tried to create the channel but I am lacking permissions')
   }
-  const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
-  let user = ""
-    if (entry.extra.channel.id === message.channel.id
-      && (entry.target.id === message.author.id)
-      && (entry.createdTimestamp > (Date.now() - 5000))
-      && (entry.extra.count >= 1)) {
+  const entry = await message.guild.fetchAuditLogs({
+    type: 'MESSAGE_DELETE'
+  }).then(audit => audit.entries.first())
+  let user = ''
+  if (entry.extra.channel.id === message.channel.id &&
+    (entry.target.id === message.author.id) &&
+    (entry.createdTimestamp > (Date.now() - 5000)) &&
+    (entry.extra.count >= 1)) {
     user = entry.executor.username
   } else {
     user = message.author.username
   }
-  accuratebotlog.sendMessage(`A message was deleted in ${message.channel.name} by ${user}`);
+  accuratebotlogs.send(`A message was deleted in ${message.channel.name} by ${user}`)
 })
 
-
 // Bot token hidden in external variable
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
