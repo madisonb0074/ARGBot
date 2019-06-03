9c3241944d732685cd075824477fd64225f944f6
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-06-02 21:55:30 -0600

    Added 8ball!

--- bot.js
+++ bot.js
@@ -47,11 +47,37 @@ client.on('message', message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
 
-  // simplest command, responds pong when user says ping w/ prefix
   switch (command) {
+    // simplest command, responds pong when user says ping w/ prefix
     case 'ping':
       message.channel.send('Pong!')
       break
+    case '8ball':
+      var eightBallResponses = [
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt.',
+        'Yes - definitely.',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Yes.',
+        'Signs point to yes.',
+        'Reply hazy, try again.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        'Don\'t count on it.',
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.',
+        'Your Pavlovian gold beast with an electrical bell overlooking opens his mouth as if to speak his mind, then shakes his head.'
+      ]
+      var randomAnswer = eightBallResponses[Math.floor(Math.random() * eightBallResponses.length)]
+      message.channel.send(randomAnswer)
   }
 })
 
