b409029ed716f3f399830a2ff8883c88e8352522
madisonb0074 <50880481+madisonb0074@users.noreply.github.com>
2019-06-03 17:45:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,5 +94,80 @@ client.on('messageDelete', function (message) {
   }
 })
 
+// sends message when important (externally editable) user statuses change (for example nickname)
+// user in a guild has been updated
+client.on('guildMemberUpdate', function (guild, oldMember, newMember) {
+  // declare changes
+  var Changes = {
+    unknown: 0,
+    addedRole: 1,
+    removedRole: 2,
+    username: 3,
+    nickname: 4,
+    avatar: 5
+  }
+  var change = Changes.unknown
+
+  // check if roles were removed
+  var removedRole = ''
+  oldMember.roles.every(function (value) {
+    if (newMember.roles.find('id', value.id) == null) {
+      change = Changes.removedRole
+      removedRole = value.name
+    }
+  })
+
+  // check if roles were added
+  var addedRole = ''
+  newMember.roles.every(function (value) {
+    if (oldMember.roles.find('id', value.id) == null) {
+      change = Changes.addedRole
+      addedRole = value.name
+    }
+  })
+
+  // check if username changed
+  if (newMember.user.username != oldMember.user.username) {
+    change = Changes.username
+  }
+  // check if nickname changed
+  if (newMember.nickname != oldMember.nickname) {
+    change = Changes.nickname
+  }
+  // check if avatar changed
+  if (newMember.user.avatarURL != oldMember.user.avatarURL) {
+    change = Changes.avatar
+  }
+  // post in the guild's log channel
+  var log = guild.channels.find('name', CHANNEL)
+  if (log != null) {
+    switch (change) {
+      case Changes.unknown:
+        log.sendMessage('**[User Update]** ' + newMember)
+        break
+      case Changes.addedRole:
+        log.sendMessage('**[User Role Added]** ' + newMember + ': ' + addedRole)
+        break
+      case Changes.removedRole:
+        log.sendMessage('**[User Role Removed]** ' + newMember + ': ' + removedRole)
+        break
+      case Changes.username:
+        log.sendMessage('**[User Username Changed]** ' + newMember + ': Username changed from ' +
+          oldMember.user.username + '#' + oldMember.user.discriminator + ' to ' +
+          newMember.user.username + '#' + newMember.user.discriminator)
+        break
+      case Changes.nickname:
+        log.sendMessage('**[User Nickname Changed]** ' + newMember + ': ' +
+          (oldMember.nickname != null ? 'Changed nickname from ' + oldMember.nickname +
+            +newMember.nickname : 'Set nickname') + ' to ' +
+          (newMember.nickname != null ? newMember.nickname + '.' : 'original username.'))
+        break
+      case Changes.avatar:
+        log.sendMessage('**[User Avatar Changed]** ' + newMember)
+        break
+    }
+  }
+})
+
 // Bot login token hidden in external variable within heroku. DO NOT PUT IN THIS FILE EVER!!!!!!!!!
 client.login(process.env.BOT_TOKEN)
